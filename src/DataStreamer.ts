e84035a0ed449e384b3daafff757f1889c736eb9
CzarT1 <cesartrocea@oakland.edu>
2023-05-05 15:56:48 -0400

    task3

--- src/DataStreamer.ts
+++ src/DataStreamer.ts
@@ -1,11 +1,40 @@
-export interface Order {
-  price: Number,
-  size: Number,
+export interface Row {
+  price_abc: number,
+  price_def: number,
+  ratio: number,
+  timestamp: date,
+  upper_bound: number,
+  lower_bound: number,
+  trigger_alert: number | unidentified,
 }
 /**
  * The datafeed server returns an array of ServerRespond with 2 stocks.
  * We do not have to manipulate the ServerRespond for the purpose of this task.
  */
+
+ export class DataStreamer {
+    static generateRow(serverRespond: ServerRespond[]): Row {
+        const priceABC = (serverRespond[0].top_ask.price + serverRespond[0].top_bid.price) / 2;
+        const priceDEF = (serverRespond[1].top_ask.price + serverRespond[1].top_bid.price) / 2;
+        const ratio = priceABC / priceDEF;
+        const upperbound = 1 + 0.05;
+        const lowerbound = 1 - 0.05;
+        return{
+            price_abc: priceABC,
+            price_def: priceDEF,
+            ratio,
+            timestamp: serverRespond[0].timestamp > serverRespond[1].timestamp ?
+                serverRespond[0].timestamp : serverRespond[1].timestamp,
+            upper_bound: upper_bound,
+            lower_bound: lowerbound,
+            trigger_alert: (ratio > upper_bound || ratio < lowerbound) ? ratio : unidentified,
+        };
+    }
+ }
+
+
+
+
 export interface ServerRespond {
   stock: string,
   top_bid: Order,
